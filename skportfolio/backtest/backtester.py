correct implementation as in Matlab with 1E-6 precision difference

skportfolio/backtest/backtester.py
@@ -21,7 +21,7 @@ def rolling_expanding_window_with_warmup(seq, warmup, n_min, n_max):
         yield win
     # roll it forward at least warmup steps
     win = deque((next(it, None) for _ in range(n_min)), maxlen=n_max)
-    yield win
+    # yield win
     for e in it:
         win.append(e)
         yield win
@@ -44,7 +44,6 @@ def sliding_dataframes(*arrays, warmup=0, n_min=0, n_max=None):
     if n_dataframes == 0:
         raise ValueError("At least one array required as input")
 
-    array_is_none = [a is None for a in arrays]
     n_samples = arrays[0].shape[0]
     for df in arrays:
         if not isinstance(df, (pd.DataFrame, pd.Series)) and df is not None:
@@ -70,29 +69,6 @@ def _make_zero_weights_with_cash(tickers):
     return weights
 
 
-def rebalance_function(current_weights, prices, estimator, **kwargs):
-    return estimator.fit(prices, **kwargs).weights_, current_weights
-
-
-def update_positions(positions, weights, current_value, t):
-    positions.loc[t] = weights * current_value
-    return positions
-
-
-def last_total_prices(prices):
-    """
-    Computes the sum of the last row of data
-    Parameters
-    ----------
-    prices
-
-    Returns
-    -------
-
-    """
-    return prices.iloc[-1:].sum()
-
-
 def find_index(current_index: pd.DatetimeIndex, full_index: pd.DatetimeIndex):
     out = np.argwhere(current_index.max() == full_index)
     if not out:
@@ -115,9 +91,11 @@ class Backtester(PortfolioEstimator):
             Tuple[int, int],
         ] = 0,
         transaction_costs: Union[float, Tuple[float, float], TransactionCostsFcn] = 0.0,
+        transactions_budget: float = 0.1,
         risk_free_rate: float = 0.0,
         rates_frequency: int = APPROX_DAYS_PER_YEAR,
         warm_start: bool = False,
+        precompute_returns: bool = True,
     ):
         """
 
@@ -160,15 +138,22 @@ class Backtester(PortfolioEstimator):
             window, regardless of the value of the RebalanceFrequency property.
             If the strategy does not require any price or signal data history, then you can indicate that the
             rebalance function requires no data by setting the window_size property to 0.
-        transaction_costs:
+        transaction_costs: float, Tuple[float,float], Callable[delta_pos, *args], default 0
             Either a fixed rate, a pair of buy-sell rates, or a more complex function with signature
             variable_transaction_costs(delta_pos, *args).
-        risk_free_rate: float
+        transactions_budget: float, default 10%
+            The maximum amount to spend in transaction costs, expressed as a percentage
+            of total initial portfolio value.
+        risk_free_rate: float, default 0.0
             The risk-free-rate. The portfolio earns this rate on the cash allocation.
         rates_frequency: int, default 252 (skportfolio._constants.APPROX_DAYS_PER_YEAR)
             The rate frequency, for annualized rates (number of business days in a year use 252)
         warm_start: bool, default False
             Whether to warm start the estimator as in many partial_fit estimators
+        precompute_returns: bool, default True
+            Whether to precompute the prices returns. When using `fit` method this is the
+            suggested behaviour to speed up calculation. Otherwise.
+
         """
 
         self.estimator = estimator
@@ -179,14 +164,16 @@ class Backtester(PortfolioEstimator):
         self.warmup_period = warmup_period
         self.window_size = window_size
         self.transaction_costs = transaction_costs
+        self.transactions_budget = transactions_budget
         self.risk_free_rate = risk_free_rate
         self.rates_frequency = rates_frequency
         self.warm_start = warm_start
+        self.precompute_returns = precompute_returns
 
         # private internals variables
         self._iteration = 0
         self._transaction_cost_fcn: Optional[Callable] = None
-        self._min_window_size: Optional[int] = 1
+        self._min_window_size: Optional[int] = 0
         self._max_window_size: Optional[int] = None
 
         # attributes to be read after fit
@@ -194,9 +181,9 @@ class Backtester(PortfolioEstimator):
         self.equity_curve_: Optional[pd.Series] = None
         self.positions_: Optional[pd.DataFrame] = None
         self.positions_at_rebalance_: Optional[pd.DataFrame] = None
-        self.rebalance_dates_: Optional[pd.DatetimeIndex] = None
+        self.rebalance_dates_: Optional[List] = []
         self.returns_: Optional[pd.Series] = None
-        self.transaction_costs_: Optional[pd.DataFrame] = None
+        self.buy_sell_costs_: Optional[pd.DataFrame] = None
         self.turnover_ = None
 
     def _cold_start(self, X, **kwargs):
@@ -206,32 +193,42 @@ class Backtester(PortfolioEstimator):
             self._min_window_size = self.window_size[0]
             self._max_window_size = self.window_size[1]
         elif isinstance(self.window_size, int):
+            # in case 0 is specified, add one for having at least one row of data, hence expanding window
             self._min_window_size, self._max_window_size = (
                 self.window_size,
                 self.n_samples,
             )
+        else:
+            raise ValueError("Not a supported window size specification")
 
         if isinstance(self.transaction_costs, (float, int)):
-            self._transaction_cost_fcn = lambda delta_pos: 2 * (
-                sum(abs(delta_pos) * self.transaction_costs),
+            self._transaction_cost_fcn = lambda delta_pos: (
+                abs(sum(delta_pos * (delta_pos > 0))) * self.transaction_costs,
+                abs(sum(delta_pos * (delta_pos < 0))) * self.transaction_costs,
             )
         elif isinstance(self.transaction_costs, (tuple, list)):
             self._transaction_cost_fcn = lambda delta_pos: (
-                sum(abs(delta_pos * self.transaction_costs[0])),
-                sum(abs(delta_pos * self.transaction_costs[1])),
+                sum(-delta_pos * (delta_pos > 0)) * self.transaction_costs[0],
+                sum(-delta_pos * (delta_pos < 0)) * self.transaction_costs[1],
             )
         elif callable(self.transaction_costs):
             self._transaction_cost_fcn = self.transaction_costs
 
+        if self.rebalance_frequency < 0:
+            raise ValueError("Rebalancing frequency must be greater or equal than 1")
+
         # Define internals initialization
         self.all_weights_ = pd.DataFrame(columns=X.columns.tolist(), dtype=float)
         self.equity_curve_ = pd.Series(name=self.name + "_equity", dtype=float)
-        self.rebalance_dates_ = []  # no rebalance yet
+        self.rebalance_dates_ = [
+            X.index[i] for i in np.arange(len(X)) if (i % self.rebalance_frequency) == 0
+        ]
+        # no rebalance yet, but the schedule is prepared
         # all the costs have been spent in buying the initial portfolio
-        self.transaction_costs_ = []
-        self.positions_ = pd.DataFrame(
-            columns=X.columns.tolist() + ["__CASH__"], dtype=float
+        self.buy_sell_costs_ = pd.DataFrame(
+            columns=["buy", "sell"], index=self.rebalance_dates_
         )
+        self.positions_ = pd.DataFrame(columns=X.columns.tolist(), dtype=float)
         self.returns_ = pd.Series(name=self.name + "_returns", dtype=float)
         # allocation is zero everywhere
         # first rebalance is considered at the first step
@@ -253,12 +250,18 @@ class Backtester(PortfolioEstimator):
         """
         # here cold restart is always done, as each call to fit must reset the previous state
         self._cold_start(X)
-
+        # all next calls to partial_fit are done like if they come from another invoker
+        self.warm_start = True
         # add the trigger rebalance column
-        for counter, (prices_slice, benchmark_slice) in enumerate(
+        if self.precompute_returns:
+            returns = X.pct_change()
+        else:
+            returns = None
+        for counter, (prices_slice, benchmark_slice, returns_slice) in enumerate(
             sliding_dataframes(
                 X,
                 y,
+                returns,
                 warmup=self.warmup_period,
                 n_min=self._min_window_size,
                 n_max=self._max_window_size,
@@ -268,39 +271,92 @@ class Backtester(PortfolioEstimator):
             self.partial_fit(
                 X=prices_slice,
                 y=benchmark_slice,
+                precomputed_returns=returns_slice,
                 full_index=X.index,
                 **kwargs,
             )
-        # self.average_turnover_ = np.mean(self.turnover_)
         # now remove the transaction costs
         return self
 
     def partial_fit(self, X, y=None, **kwargs):
+        """
+        The method to be repeatedly called on slices of the price data.
+        Parameters
+        ----------
+        X: pd.DataFrame
+            A slice of prices data
+        y: pd.Series
+            Any additional variable needed to the estimator `fit` method
+        kwargs:
+            Any additional argument to pass to the estimator `fit` method, together
+            with the full_index variable that describes the full temporal interval
+            the X data come from.
+
+        Returns
+        -------
+
+        """
         full_index = kwargs["full_index"]
+
+        X_rets = None
+        if "precomputed_returns" in kwargs:
+            X_rets = kwargs["precomputed_returns"]
+        else:
+            X_rets = X.pct_change()
+        last_index = X.index[-1]
         self._iteration = find_index(X.index, full_index=full_index)
+
+        needs_rebalance = (self._iteration > 0) and (
+            last_index in self.rebalance_dates_
+        )
         if not self.warm_start and self._iteration == 0:
             self._cold_start(X)
-
-        last_index = X.index[-1]
 
         if self.initial_weights is None:
             self.initial_weights = self.estimator.fit(X).weights_
             self.all_weights_.loc[last_index, :] = self.initial_weights
-        else:
+        elif self.initial_weights is not None and not needs_rebalance:
             self.all_weights_.loc[last_index, :] = self.initial_weights
+        elif self.initial_weights is not None and needs_rebalance:
+            self.all_weights_.loc[last_index, :] = self.estimator.fit(X).weights_
 
-        # here we use tail(2) to avoid unnecessary computations
-        self.returns_.loc[last_index] = (
-            X.tail(2).pct_change().tail(1).dot(self.all_weights_.loc[last_index]).iat[0]
+        self.returns_.loc[last_index] = X_rets.loc[last_index].dot(
+            self.all_weights_.loc[last_index]
         )
-        # TODO this computation can be made faster with some simplification
-        # we keep this because it exactly corresponds to Matlab
-        self.equity_curve_.loc[last_index] = self.initial_portfolio_value * np.exp(
-            np.log1p(self.returns_).sum()
+        # TODO this should be made faster through caching of intermediate results
+        self.positions_ = (
+            (1 + X_rets)
+            .cumprod()
+            .fillna(1)
+            .mul(self.initial_portfolio_value)
+            .rmul(self.all_weights_.iloc[-1, :])
         )
-        self.positions_.loc[last_index] = (
-            self.equity_curve_.loc[last_index] * self.all_weights_.loc[last_index]
-        )
+        self.equity_curve_ = self.positions_.sum(1)
+
+        if needs_rebalance:
+            # we must ensure that our data slice contains the index of previous rebalance
+            # if this is not true because our sliding window is too small we need to
+            # 1. either do nothing
+            # 2. inform the user with a warning, to increase the time window or to switch to
+            # expanding window
+            prev_index = self.rebalance_dates_[
+                self.rebalance_dates_.index(last_index) - 1
+            ]
+            if prev_index in self.positions_.index:
+                delta_pos = (
+                    self.positions_.loc[last_index, :]
+                    - self.positions_.loc[prev_index, :]
+                )
+                buy_cost, sell_cost = self._transaction_cost_fcn(delta_pos)
+                self.buy_sell_costs_.loc[last_index] = (
+                    buy_cost,
+                    sell_cost,
+                )
+                self.positions_.loc[last_index] -= buy_cost + sell_cost
+                self.equity_curve_.loc[last_index] -= buy_cost + sell_cost
+                self.returns_.loc[last_index] = (
+                    self.equity_curve_.tail(2).pct_change().iat[-1]
+                )
 
         return self
 